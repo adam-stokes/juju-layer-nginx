cf55b0e0bb1c14949983c8ae9670984c17fa59bc
Adam Stokes <adam.stokes@ubuntu.com>
2015-11-04 15:45:39 -0500

    update hooks
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- reactive/nginx.py
+++ reactive/nginx.py
@@ -50,26 +50,27 @@ def configure_site(site, context):
            })
 
 
-# HOOKS -----------------------------------------------------------------------
+# REACTORS --------------------------------------------------------------------
 @hook('install')
 def install():
-    """ Install dependencies for application
+    """ Install nginx
     """
     hookenv.status_set('maintenance', 'Installing NGINX')
+
     # Install nginx-full
     if is_state('nginx.available'):
         return
 
     apt_install(['nginx-full'])
 
-    # Perform our application install
     set_state('nginx.available')
 
 
 @hook('config-changed')
 def config_changed():
     config = hookenv.config()
-    if not is_state('nginx.available') or not config.changed('nginx-port'):
+
+    if not is_state('nginx.available'):
         return
 
     hookenv.log('Updating NGINX vhosts', 'info')
@@ -82,7 +83,6 @@ def config_changed():
     hookenv.status_set('maintenance', '')
 
 
-# REACTORS --------------------------------------------------------------------
 @when('nginx.start')
 @when_not('nginx.started')
 def start_nginx():
